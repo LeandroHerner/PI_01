ea13e3b9bdcc8dd7b1f78f0f7a960d1850df7ee2
LeandroHerner <di.leandro.herner@gmail.com>
2023-12-28 17:29:57 -0300

    limpieza archivos entrega

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import json
-import ast
-#import warnings
-#warnings.filterwarnings('ignore')
-import pyarrow as pa
-import pyarrow.parquet as pq
+#import json
+#import ast
+#import pyarrow as pa
+#import pyarrow.parquet as pq
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
@@ -18,6 +16,10 @@ app = FastAPI()
 # Direccion del web service Render
 # https://pi-01-api-lh.onrender.com/docs#/
 
+
+
+
+
 # Se cargan los archivos parquet a utilizar
 steam_games = pd.read_parquet('datos_parquet/steam_games.parquet')
 users_items = pd.read_parquet('datos_parquet/user_items_chunk.parquet')
@@ -25,45 +27,55 @@ user_reviews = pd.read_parquet('datos_parquet/user_reviews.parquet')
 user_sim = pd.read_parquet('datos_parquet/user_sim.parquet')
 item_recortado = pd.read_parquet('datos_parquet/item_chunk.parquet')
 
-#### PRIMERA CONSULTA ####
 
-# Creo dos variables en las que se almacenan dos dataframes con las columnas a utilizar para la consulta
 
+
+
+#### PRIMERA CONSULTA (PlayTimeGenre) ####
+
+# Se crean dos variables en las que se almacenan dos dataframes con las columnas a utilizar para la consulta
 items_genre = users_items[['id', 'item_name', 'playtime_forever']]
-
 games_genre = steam_games[['id', 'genres', 'year']]
 
 # Se crea un dataframe realizando un merge entre las variables anteriores utilizando la columna 'id' que tienen en comun.
 playTimeGenre = pd.merge(items_genre, games_genre, on='id', how='inner')
 
-def obtener_año_max_horas(consulta):
-    # Filtro el DataFrame con la consulta
-    genero = playTimeGenre[playTimeGenre['genres'] == consulta]
-
-    # Agrupo por año y suma las horas jugadas
-    resultados = genero.groupby('year')['playtime_forever'].sum().reset_index()
-
-    # Encuentra el año con más horas jugadas
-    año_max_horas = resultados.loc[resultados['playtime_forever'].idxmax()]
-
-    # Devuelve el resultado como un diccionario
-    return {"genero": consulta, "año_max_horas": {"año": int(año_max_horas['year'])}} #NECESITO LA RESPUESTA EN FORMATO STR
-    
-    #return {f"Para el género {consulta}, el año con más horas jugadas es {int(año_max_horas['year'])}"}
-    
-    #return ConsultaResultado(mensaje=retorno)
-
-
-# Primer consulta de la API
+# Se establece la ruta de la funcion
 @app.get("/PlayTimeGenre/{genero}", response_model=dict)
 
-def consulta_año_max_horas(genero: str):
-    retorno = obtener_año_max_horas(genero)
-    return retorno
+# Se ingresa un genero y devuelve el año que mas horas jugadas tuvo
+def PlayTimeGenre(consulta : str):
+    
+    # Convertir la consulta a minúsculas
+    consulta = consulta.lower()
+    
+    # Se filtra el DataFrame con la consulta
+    genero = playTimeGenre[playTimeGenre['genres'] == consulta]
+
+    # Se verifica si hay datos para el género dado
+    if not genero.empty:
+        
+        # Se agrupa por año y suma las horas jugadas
+        resultados = genero.groupby('year')['playtime_forever'].sum().reset_index()
+
+        # Se encuentra el año con más horas jugadas
+        if not resultados.empty:
+            año_max_horas = resultados.loc[resultados['playtime_forever'].idxmax()]
+
+            # Devuelve el resultado como un diccionario
+            return {"genero": consulta, "año_max_horas": {"año": int(año_max_horas['year'])}}
+        else:
+            return {"mensaje": f"No hay datos disponibles para el género {consulta} en ningún año."}
+    else:
+        return {"mensaje": f"No hay datos disponibles para el género {consulta} o el género no existe."}
 
 
-#### SEGUNDA CONSULTA #### 
 
+
+
+#### SEGUNDA CONSULTA (UserForGenre) #### 
+
+# Se definen clases que van a ser utiles a la segunda funcion
 class UsuarioMasJugador(BaseModel):
     nombre: str
     horas_acumuladas: int
@@ -75,17 +87,23 @@ class AcumulacionPorAño(BaseModel):
 class ConsultaResultado(BaseModel):
     usuario_mas_jugado: UsuarioMasJugador
     acumulacion_por_año: List[AcumulacionPorAño]
+    
 
-
+# Se crean dos variables en las que se almacenan dos dataframes con las columnas a utilizar para la consulta
 items_UserForGenre = users_items[['id', 'user_id', 'playtime_forever']]
 
 games_UserForGenre = steam_games[['id', 'genres', 'year']]
 
+# Se crea un dataframe realizando un merge entre las variables anteriores utilizando la columna 'id' que tienen en comun.
 UserForGenre = pd.merge(items_UserForGenre, games_UserForGenre, on='id', how='inner')
 
+# Se establece la ruta de la funcion
+@app.get("/UserForGenre/{genero}", response_model=ConsultaResultado)
 
-
-def obtener_año_y_usuario_max_horas(consulta):
+def User_For_Genre(consulta : str):
+    
+    # Convertir la consulta a minúsculas
+    consulta = consulta.lower()
 
     #Filtro el dataFrame con la consulta
     genero = UserForGenre[UserForGenre['genres'] == consulta]
@@ -106,84 +124,82 @@ def obtener_año_y_usuario_max_horas(consulta):
 
 
 
-# Segunda consulta de la API
-
-@app.get("/UserForGenre/{genero}", response_model=ConsultaResultado)
-def consulta_año_y_usuario_max_horas(genero: str):
-    resultado = obtener_año_y_usuario_max_horas(genero)
-    return resultado
 
 
+#### TERCERA CONSULTA (UsersRecommend) ####
 
-
-#### TERCERA CONSULTA ####
-
+# Se almacena en un dataframe las columnas a utilizar para la consulta
 games_UsersRecommend = steam_games[['id', 'title']]
 
 UsersRecommend = pd.merge(user_reviews, games_UsersRecommend, on='id', how='inner')
 
-def obtener_top3_positivo(year):
-    
-    # filtro por año en primera instancia
-    UsersRecommend_filtrado_anio = UsersRecommend[UsersRecommend['review_year'] == year]
-
-    # luego filtro contemplando solo las filas que cumplen con ambos booleanos:
-    # ser True en la columna 'recommend' y tener valor 1 o 2 en la columna 'sentiment_analysis'
-    UsersRecommend_filtrado = UsersRecommend_filtrado_anio[(UsersRecommend_filtrado_anio['recommend'] == True) & (UsersRecommend_filtrado_anio['sentiment_analysis'].isin([1, 2]))]
-
-    # Agrupar por id de juego y sumar las recomendaciones
-    top_games = UsersRecommend_filtrado.groupby('id').agg({'title': 'first', 'recommend': 'sum'}).reset_index()
-
-    # Ordenar por el número de recomendaciones en orden descendente
-    top_games_sorted = top_games.sort_values(by='recommend', ascending=False)
-
-    top3Games = top_games_sorted.head(3)
-
-    resultado = [{"Top 1": {top3Games.iloc[0]['title']}}, {"Top 2": {top3Games.iloc[1]['title']}}, {"Top 3": {top3Games.iloc[2]['title']}}]
-    
-    return resultado
-    # Devolver el resultado como un diccionario
-    #return {"top3": top3Games[['id', 'title', 'recommend']].to_dict(orient='records')}
-
 # Definir la ruta en FastAPI
 @app.get("/UsersRecommend/{year}")
-def obtener_top3(year: int):
-    resultado = obtener_top3_positivo(year)
-    return resultado
 
 
-
-#### CUARTA CONSULTA ####
-
-def obtener_top3_negativo(year):
+def Users_Recommend(year : int):
     
-    # filtro por año en primera instancia
-    UsersRecommend_filtrado_anio = UsersRecommend[UsersRecommend['review_year'] == year]
 
-    # luego filtro contemplando solo las filas que cumplen con ambos booleanos:
-    # ser True en la columna 'recommend' y tener valor 1 o 2 en la columna 'sentiment_analysis'
-    UsersRecommend_filtrado = UsersRecommend_filtrado_anio[(UsersRecommend_filtrado_anio['recommend'] == False) & (UsersRecommend_filtrado_anio['sentiment_analysis'].isin([0]))]
+    if 2010 <= year <= 2015:
+        # filtro por año en primera instancia
+        UsersRecommend_filtrado_anio = UsersRecommend[UsersRecommend['review_year'] == year]
 
-    # Agrupar por id de juego y sumar las recomendaciones
-    top_games = UsersRecommend_filtrado.groupby('id').agg({'title': 'first', 'recommend': 'sum'}).reset_index()
+        # luego filtro contemplando solo las filas que cumplen con ambos booleanos:
+        # ser True en la columna 'recommend' y tener valor 1 o 2 en la columna 'sentiment_analysis'
+        UsersRecommend_filtrado = UsersRecommend_filtrado_anio[(UsersRecommend_filtrado_anio['recommend'] == True) & (UsersRecommend_filtrado_anio['sentiment_analysis'].isin([1, 2]))]
 
-    # Ordenar por el número de recomendaciones en orden descendente
-    top_games_sorted = top_games.sort_values(by='recommend', ascending=True)
+        # Agrupar por id de juego y sumar las recomendaciones
+        top_games = UsersRecommend_filtrado.groupby('id').agg({'title': 'first', 'recommend': 'sum'}).reset_index()
 
-    top3Games = top_games_sorted.head(3)
+        # Ordenar por el número de recomendaciones en orden descendente
+        top_games_sorted = top_games.sort_values(by='recommend', ascending=False)
 
-    resultado = [{"Top 1": {top3Games.iloc[0]['title']}}, {"Top 2": {top3Games.iloc[1]['title']}}, {"Top 3": {top3Games.iloc[2]['title']}}]
-    
-    return resultado
+        top3Games = top_games_sorted.head(3)
+
+        resultado = [{"Top 1": {top3Games.iloc[0]['title']}}, {"Top 2": {top3Games.iloc[1]['title']}}, {"Top 3": {top3Games.iloc[2]['title']}}]
+        
+        return resultado
+    else:
+        return "El año ingresado está fuera del periodo estudiado o es invalido"
+
+
+
+
+
+#### CUARTA CONSULTA (UsersNotRecommend) ####
 
 # Definir la ruta en FastAPI
 @app.get("/UsersNotRecommend/{year}")
-def obtener_top3(year: int):
-    resultado = obtener_top3_negativo(year)
-    return resultado
+
+def Users_Not_Recommend(year : int):
+    
+    if 2010 <= year <= 2015:
+        # filtro por año en primera instancia
+        UsersRecommend_filtrado_anio = UsersRecommend[UsersRecommend['review_year'] == year]
+
+        # luego filtro contemplando solo las filas que cumplen con ambos booleanos:
+        # ser True en la columna 'recommend' y tener valor 1 o 2 en la columna 'sentiment_analysis'
+        UsersRecommend_filtrado = UsersRecommend_filtrado_anio[(UsersRecommend_filtrado_anio['recommend'] == False) & (UsersRecommend_filtrado_anio['sentiment_analysis'].isin([0]))]
+
+        # Agrupar por id de juego y sumar las recomendaciones
+        top_games = UsersRecommend_filtrado.groupby('id').agg({'title': 'first', 'recommend': 'sum'}).reset_index()
+
+        # Ordenar por el número de recomendaciones en orden descendente
+        top_games_sorted = top_games.sort_values(by='recommend', ascending=True)
+
+        top3Games = top_games_sorted.head(3)
+
+        resultado = [{"Top 1": {top3Games.iloc[0]['title']}}, {"Top 2": {top3Games.iloc[1]['title']}}, {"Top 3": {top3Games.iloc[2]['title']}}]
+        
+        return resultado
+    else:
+        return "El año ingresado está fuera del periodo estudiado o es invalido"
 
 
-#### QUINTA CONSULTA ####
+
+
+
+#### QUINTA CONSULTA (sentiment_analysis) ####
 
 games_sentiment = steam_games[['id', 'title', 'year']]
 
@@ -193,38 +209,45 @@ sentiment = pd.merge(user_sentiment, games_sentiment, on='id', how='inner')
 
 
 @app.get("/sentiment_analysis/{year}")
-def obtener_sentimiento_analisis_por_año(year: int):
+def sentiment_analysis(year: int):
+    
     # Filtrar por año específico
     df_filtrado = sentiment[sentiment['year'] == year]
 
-    # Verificar si hay datos después del filtrado
+    # Se verifica si hay datos después del filtrado
     if df_filtrado.empty:
-        return {"message": "No hay datos para el año dado."}
-
-    # Contar la cantidad de registros por categoría de análisis de sentimiento
-    cantidad_sentimientos = df_filtrado['sentiment_analysis'].value_counts().to_dict()
+        return {"No hay datos para el año dado."}
     
-    # Mapeo los valores a los nombres deseados
-    cantidad_sentimientos_nombres = {
-        "Positivo": cantidad_sentimientos.get(2, 0),
-        "Neutral": cantidad_sentimientos.get(1, 0),
-        "Negativo": cantidad_sentimientos.get(0, 0)}
+    # Se filtra de acuerdo al periodo 2010/2015
+    elif 2010 <= year <= 2015:
+        # Contar la cantidad de registros por categoría de análisis de sentimiento
+        cantidad_sentimientos = df_filtrado['sentiment_analysis'].value_counts().to_dict()
+        
+        # Mapeo los valores a los nombres deseados
+        cantidad_sentimientos_nombres = {
+            "Positivo": cantidad_sentimientos.get(2, 0),
+            "Neutral": cantidad_sentimientos.get(1, 0),
+            "Negativo": cantidad_sentimientos.get(0, 0)}
 
-    # Devolver el resultado como un diccionario
-    return {"sentiment_analysis": cantidad_sentimientos_nombres}
+        # Devolver el resultado como un diccionario
+        return {"sentiment_analysis": cantidad_sentimientos_nombres}
+    else:
+        return "El año ingresado es invalido"
 
 
-#### SEXTA CONSULTA ####
+
+
+
+#### SEXTA CONSULTA (recomendacion_juego) ####
 
 @app.get("/recomendacion_juego/{item_id}")
-def cinco_recomendaciones2(item_id):
-    item_id=int(item_id)
-    # Se verifica si el ID de juego proporcionado existe en el DataFrame
+def modelo_de_recomendacion(item_id : int):
+    
     if item_id not in item_recortado['item_id'].values:
         return f"Error: El ID de juego {item_id} no se encuentra en el DataFrame."
     
     # Verificar si hay suficientes datos para calcular recomendaciones
-    if len(user_sim) <= item_id:
+    elif len(user_sim) <= item_id:
         return "Error: No hay suficientes datos para calcular recomendaciones."
     
     else:
@@ -236,7 +259,4 @@ def cinco_recomendaciones2(item_id):
         encabezado = f'Juegos similares a {item_name}:'
         
         user_sim_val = user_sim.sort_values(by=item_name, ascending=False).index[1:6]
-        #[i for i in user_sim_val]
-        #for item in user_sim_val:
-        #    encabezado += f'\n-{item}'
         return encabezado, [i for i in user_sim_val]
